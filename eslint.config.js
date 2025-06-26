fbf29d0080ec03bdc7023931b6754ec662d563b9
Bakr101 <ahmed98bakr@gmail.com>
2025-06-27 01:34:41 +0300

    security fail

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,15 @@
-import js from "@eslint/js";
+import pluginJs from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
 
+/** @type {import('eslint').Linter.Config[]} */
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
 ]);
